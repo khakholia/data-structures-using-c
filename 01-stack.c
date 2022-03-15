b36b9724b5ce9e0a25d604b6e8087533780f7656
nikitkhakholia <khakholia.nk@gmail.com>
2022-03-15 15:05:25 +0530

    program1

--- 01-stack.c
+++ 01-stack.c
@@ -4,7 +4,8 @@ struct Node{
     int data;
     struct Node *link;
 };
-
+int stack[100];
+int stackTop=0;
 struct Node *top; 
 void push(){
     struct Node *new = (struct Node*)malloc(sizeof(struct Node));  
@@ -24,7 +25,9 @@ void pop(){
         printf("Empty!!\n");
     }else{
         int val = top->data;
+        struct Node *toBeFreed=top;
         top = top->link;
+        free(toBeFreed);
         printf("Popped!!\n");
     }
 }
@@ -41,15 +44,66 @@ void display(){
         }
     }
 }
-void main(){
+void pushIntoArray(){
+    if(stackTop==100){
+        printf("Full!!\n");
+    }else{
+        printf("\nEnter the number: ");
+        scanf("%d",&stack[stackTop]);
+        stackTop++;
+    }
+}
+void popFromArray(){
+    if(stackTop==0){
+        printf("Empty!!\n");
+    }else{
+        stackTop--;
+    }
+}
+void displayArrayStack(){
+    printf("\nElements!!\n");
+    for(int i=(stackTop-1); i>=0;i--){
+        printf("%d\n",stack[i]);
+    }
+}
+void usingArray(){
+    int choice1=-1;
+    printf("\nStack (Array)");
+    while(choice1!=0){
+        printf("\nChoose any one\n0 Exit\n1 Push\n2 Pop\n3 Traverse\nSelect:- ");
+        scanf("%d",&choice1);
+        switch (choice1)
+        {
+            case 0:{
+                return;
+            }
+            case 1:{
+                pushIntoArray();
+                break;
+            }
+            case 2:{
+                popFromArray();
+                break;
+            }
+            case 3:{
+                displayArrayStack();
+                break;
+            }
+            default:{
+                printf("\nPlease enter between 0-3 only!!!\n");
+            }
+        }
+    }
+}
+void usingLL(){
     int choice=-1;
-    printf("Stack (Linked List)");
+    printf("\nStack (Linked List)");
     while(choice!=0){
         printf("\nChoose any one\n0 Exit\n1 Push\n2 Pop\n3 Traverse\nSelect:- ");
         scanf("%d",&choice);
         switch(choice){
             case 0:{
-                exit(0);
+                return;
             }
             case 1:{
                 push();
@@ -68,4 +122,30 @@ void main(){
             }
         }
     }
+}
+void main(){
+    int select=-1;
+    while (select!=0)
+    {
+        printf("\nChoose any one\n0 Exit\n1 Linked List\n2 Array\nSelect:- ");
+        scanf("%d",&select);
+        switch (select)
+        {
+        case 0:
+            exit(0);
+            break;
+        case 1:
+            usingLL();
+            break;
+        case 2:
+            usingArray();
+            break;
+        default:
+            printf("\nPlease enter between 0-2 only!!!\n");
+        }
+    }
+    
+    
+
+    
 }
